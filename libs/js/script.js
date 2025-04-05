fe2d24b608f34560e91f356d3b8aa0139cfd75fb
MikailMichael <mdeveden95@gmail.com>
2025-04-05 14:19:42 +0100

    Completed Gazetteer Project

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,14 +1,108 @@
+/*********************************************/
+/*            Initialize Leaflet             */
+/*********************************************/
+
+let map = L.map('map').setView([38.8951100, -77.0363700], 10);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+L.easyButton("fa-info fa-xl", function (btn, map) {
+	$("#generalInfo").modal("show");
+}).addTo(map);
+
+L.easyButton("fa-solid fa-person fa-xl", function (btn, map) {
+	$("#populationInfo").modal("show");
+}).addTo(map);
+
+L.easyButton("fa-solid fa-cloud-sun fa-xl", function (btn, map) {
+	$("#weatherInfo").modal("show");
+}).addTo(map);
+
+L.easyButton("fa-solid fa-coins fa-xl", function (btn, map) {
+	$("#currencyInfo").modal("show");
+}).addTo(map);
+
+L.easyButton("fa-solid fa-flag fa-xl", function (btn, map) {
+	$("#flagInfo").modal("show");
+}).addTo(map);
+
+
+/*********************************************/
+/*    Executes api call for OpenCage API     */
+/*********************************************/
+
+// Sets Location
+function setLocation(lat, lng) {
+	$.ajax({
+		url: "libs/php/reverseGeocode.php",
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			lat: lat,
+			lng: lng
+		},
+		// Output stored in the object result if successful
+		success: function(result) {
+
+			console.log(JSON.stringify(result["data"][0]["components"]));
+			// If status is ok, data within result is writtin into the HTML using jQuery functions
+			if (result.status.code == "200") {
+				$('#country').html(result["data"][0]["components"]["country"]);
+			}
+      if (result["data"][0]["components"]["state"]) {
+        $('#state').html(result["data"][0]["components"]["state"]);
+      }
+      if (result["data"][0]["components"]["city"]) {
+        $('#city').html(result["data"][0]["components"]["city"]);
+      }
+
+      map.setView([lat, lng], 13);
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.log("Error has occured with call to OpenCage API.");
+		}
+	});
+}
+
+// Success function for retrieving current location
+function success(pos) {
+  const lat = pos.coords.latitude;
+  const lng = pos.coords.longitude;
+  console.log(`Latitude : ${lat}`);
+  console.log(`Longitude: ${lng}`);
+  console.log(`More or less ${pos.coords.accuracy} meters.`);
+  setLocation(lat, lng);
+}
+
+// Error handling function for retrieving current location
+function error(err) {
+  console.log(`ERROR(${err.code}): ${err.message}`);
+}
+
+/***************************************************/
+/* Executes preloader & retrieves current location */
+/***************************************************/
 
 $(window).on('load', function () {
+  // Preloader
 	if ($('#preloader').length) {
 		$('#preloader').delay(1000).fadeOut('slow', function () {
 			$(this).remove();
 		});
 	}
+
+  // Retrieve user location
+  navigator.geolocation.getCurrentPosition(success, error);
 });
+
 /*********************************************/
-/*    Executes api call for Timezone api     */
+/*              Temp Example                 */
 /*********************************************/
+
+/*
 $('#btnTZ').click(function() {
 	$.ajax({
 		url: "libs/php/getTimeZoneInfo.php",
@@ -49,79 +143,4 @@ $('#btnTZ').click(function() {
 		}
 	});
 });
-
-/*********************************************/
-/*     Executes api call for Ocean api       */
-/*********************************************/
-$('#btnOcean').click(function() {
-	$.ajax({
-		url: "libs/php/getOceanInfo.php",
-		type: 'POST',
-		dataType: 'json',
-		data: {
-			lat: $('#oceanLat').val(),
-			lng: $('#oceanLng').val()
-		},
-		// Output stored in the object result if successful
-		success: function(result) {
-
-			console.log(JSON.stringify(result));
-			// If status is ok, data within result is writtin into the HTML using jQuery functions
-			if (result.status.name == "ok" && result.data.ocean) {
-				$('#name').html(result['data']['ocean']['name']);
-				$('#distance').html(result['data']['ocean']['distance']);
-				$('#geonameId').html(result['data']['ocean']['geonameId']);
-			} else if (result.data.status) {
-				$('#name').html(result['data']['status']["message"]);
-				$('#distance').html('');
-				$('#geonameId').html('');
-			}
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-			// your error code
-			console.log("Error has occured with call to API.");
-		}
-	});
-});
-
-/*********************************************/
-/*   Executes api call for Earthquake api    */
-/*********************************************/
-$('#btnQuake').click(function() {
-	$.ajax({
-		url: "libs/php/getQuakeInfo.php",
-		type: 'POST',
-		dataType: 'json',
-		data: {
-			north: $('#north').val(),
-			south: $('#south').val(),
-			east: $('#east').val(),
-			west: $('#west').val()
-		},
-		// Output stored in the object result if successful
-		success: function(result) {
-
-			console.log(JSON.stringify(result));
-			// If status is ok, data within result is writtin into the HTML using jQuery functions
-			if (result.status.name == "ok" && result.data.earthquakes[0]) {
-				$('#firstMag').html(result['data']['earthquakes'][0]['magnitude']);
-				$('#firstDate').html(result['data']['earthquakes'][0]['datetime']);
-				$('#secondMag').html(result['data']['earthquakes'][1]['magnitude']);
-				$('#secondDate').html(result['data']['earthquakes'][1]['datetime']);
-				$('#thirdMag').html(result['data']['earthquakes'][2]['magnitude']);
-				$('#thirdDate').html(result['data']['earthquakes'][2]['datetime']);	
-			} else if (!result.data.earthquakes[0]) {
-				$('#firstMag').html('');
-				$('#firstDate').html('There are no recorded earthquakes in the set area.');
-				$('#secondMag').html('');
-				$('#secondDate').html('');
-				$('#thirdMag').html('');
-				$('#thirdDate').html('');	
-			}
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-			// your error code
-			console.log("Error has occured with call to API.");
-		}
-	});
-});
+*/
